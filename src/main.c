d74cb5af7e53668c92d0e832a50b612eb3c4e730
jugurtha bessaoud <jugurtha.bessaoud@ge1.iut-cachan.u-psud.fr>
2018-11-13 09:45:30 +0100

    Fin exo2

--- src/main.c
+++ src/main.c
@@ -1,9 +1,13 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
-
 #include "my_header.h"
 
-int main(void)
-{
+int main(void){
+    unsigned long long int S1 = 1;
+    unsigned char n = 2;
+
+    while (n<50){
+        S1 = S1 * n;
+        n++;
+    }
+
     return 0;
 }
