15c54062937b3336f8f6257fe2ddc58f8a6cf1f7
jbessaoud <jugurtha.bessaoud@u-psud.fr>
2018-11-14 22:33:14 +0100

    fin exo 45

--- src/main.c
+++ src/main.c
@@ -1,11 +1,38 @@
 #include "my_header.h"
 #include "TP_lib.h"
+#include <stdio.h>
 
-void main(void){
-	short resultat , i=0;
+int main(void){
+	short int points=0 , i=0;
 
-	while(i<10){
-		resultat=LancerDe();
-		i++;
+	printf("C'est votre tour.");
+	i= LancerDe();
+
+	if(i==1){
+		printf("Fin de votre tour.");
+	}
+
+	else{
+	    while(i!=1){
+            if(i==2 || i==4 || i==6){
+                points=points+i;
+            }
+            
+            else if(i==3){
+                points=points*3;
+            }
+            
+            else if(i==5 && points>=2){
+                points=points-2;
+            }
+
+            else if(i==5 && points<2){
+                points=0;
+            }
+            i=LancerDe();
+	    }
+
+	printf("Fin de votre tour. Votre score est de %d",points);
+	return 0;
 	}
 }
