48f8161eaa033f92249ce2ef7281adeee93fc38b
jbessaoud <44801362+jbessaoud@users.noreply.github.com>
2018-12-11 11:38:47 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,17 @@
-//  Copyright (c) 2018 Antoine Tran Tan
-//
+#include <ansi_c.h>
+#include "math.h"
+#include "biblio_resistance.h"
 
-#include "my_header.h"
-
-int main(void)
-{
-    return 0;
+int main(){
+	long int anneau1, anneau2, anneau3, val_res;
+	
+	anneau1=saisir_anneau(0,9,1);
+	anneau2=saisir_anneau(0,9,2);
+	anneau3=saisir_anneau(0,6,3);
+	
+	val_res=calculer_val_resistance(anneau1,anneau2,anneau3);
+	
+	afficher_resistance(val_res);
+	
+	return 0;
 }
